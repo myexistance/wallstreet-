9d9d7b22d78d5447225eba52c938c2b9538ddc5a
myexistance <joonyoonlives@gmail.com>
2019-06-24 17:22:42 +1200

    about-template added and css adsusted for css page

---
+++ about-template.php
@@ -0,0 +1,72 @@
+<?php
+    /* Template Name: About Template */
+?>
+<?php get_header(); ?>
+
+
+
+
+
+  <div id="postList" class="row cardList">
+    <?php if( have_posts() ): ?>
+      <main class="container" role="main">
+
+      <div class="col <?php echo $contentorder; ?>">
+
+        <div id="postList" class="row cardList">
+
+        <?php while( have_posts() ): the_post() ?>
+
+            <div class="card col-3 col-sm-8 col-md-12 mr-auto ml-auto mb-2">
+
+
+
+
+
+              <h5 class="textCenter mb-4"><?php the_title(); ?></h5>
+              <div class="center mb-4">
+              <img class="floatLeft"><?php the_post_thumbnail(); ?>
+              </div>
+              <div class="content">
+                  <?php the_content(); ?>
+              </div>
+
+              
+
+          </div>
+
+        <?php endwhile; ?>
+
+        <?php else: ?>
+            <p>There is no post</p>
+
+
+          </div>
+          </div>
+
+        <?php endif; ?>
+
+
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="container">
+  <p class="float-right"><a href="#">Back to top</a></p>
+  <p>&copy; 2019 Hanjan Design Company, Inc. &middot; <a href="https://myexistance.github.io/Joon.portFolio/">PortFolio</a> &middot;</
+</footer>
+</main>
